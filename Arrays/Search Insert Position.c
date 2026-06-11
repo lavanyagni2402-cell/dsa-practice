03d29f02f1a744037be080d102a5cf5df1fde4a2
lavanyagni2402-cell <lavanyagni2402@gmail.com>
2026-06-11 23:47:27 +0530

    Search Insert Position

---
+++ Arrays/Search Insert Position.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int searchInsert(int* nums, int numsSize, int target) {
+    int left = 0;
+    int right = numsSize - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] == target)
+            return mid;
+        else if (nums[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+
+    return left;
+}
+
+int main() {
+    int nums[] = {1, 3, 5, 6};
+    int target = 5;
+    int numsSize = sizeof(nums) / sizeof(nums[0]);
+
+    int position = searchInsert(nums, numsSize, target);
+
+    printf("Position = %d\n", position);
+
+    return 0;
+}
