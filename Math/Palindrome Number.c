82ff2148e055f2eb195b29afb950c335d53eb92d
lavanyagni2402-cell <lavanyagni2402@gmail.com>
2026-06-19 23:37:14 +0530

    Palindrome Number

---
+++ Math/Palindrome Number.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isPalindrome(int x) {
+    if (x < 0)
+        return false;
+
+    long rev = 0;
+    int original = x;
+
+    while (x > 0) {
+        rev = rev * 10 + x % 10;
+        x /= 10;
+    }
+
+    return original == rev;
+}
+
+int main() {
+    int x = 121;
+
+    if (isPalindrome(x))
+        printf("true\n");
+    else
+        printf("false\n");
+
+    return 0;
+}
