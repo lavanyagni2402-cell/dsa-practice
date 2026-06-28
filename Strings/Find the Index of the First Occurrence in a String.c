2eea0d662710cdbabb982c91059b1418a6ceeb8f
lavanyagni2402-cell <lavanyagni2402@gmail.com>
2026-06-28 22:07:53 +0530

    LeetCode 28- Find the Index of the First Occurrence in a String

---
+++ Strings/Find the Index of the First Occurrence in a String.c
@@ -0,0 +1,33 @@
+#include <string.h>
+#include <stdio.h>
+
+int strStr(char *haystack, char *needle);
+
+int main() {
+    char haystack[] = "hello";
+    char needle[] = "ll";
+
+    printf("%d\n", strStr(haystack, needle));
+
+    return 0;
+}
+
+int strStr(char *haystack, char *needle) {
+    int n = strlen(haystack);
+    int m = strlen(needle);
+
+    if (m == 0)
+        return 0;
+
+    for (int i = 0; i <= n - m; i++) {
+        int j = 0;
+
+        while (j < m && haystack[i + j] == needle[j])
+            j++;
+
+        if (j == m)
+            return i;
+    }
+
+    return -1;
+}
