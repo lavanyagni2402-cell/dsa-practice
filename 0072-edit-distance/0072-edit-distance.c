1204cace4a3776bbdb8c99aa5a7b3ea378d32808
Lavanya Agnihotri <lavanyagni2402@gmail.com>
2026-07-23 22:58:18 +0530

    Time: 7 ms (15.32%), Space: 11.7 MB (9.28%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int min(int a, int b) {
+    return (a < b) ? a : b;
+}
+
+int minDistance(char* word1, char* word2) {
+    int m = strlen(word1);
+    int n = strlen(word2);
+
+    int **dp = (int **)malloc((m + 1) * sizeof(int *));
+    for (int i = 0; i <= m; i++) {
+        dp[i] = (int *)malloc((n + 1) * sizeof(int));
+    }
+
+    // Base cases
+    for (int i = 0; i <= m; i++)
+        dp[i][0] = i;
+
+    for (int j = 0; j <= n; j++)
+        dp[0][j] = j;
+
+    // Fill DP table
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+
+            if (word1[i - 1] == word2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1];
+            } else {
+
+                int insert = dp[i][j - 1];
+                int delete = dp[i - 1][j];
+                int replace = dp[i - 1][j - 1];
+
+                dp[i][j] = 1 + min(insert, min(delete, replace));
+            }
+        }
+    }
+
+    int ans = dp[m][n];
+
+    for (int i = 0; i <= m; i++)
+        free(dp[i]);
+    free(dp);
+
+    return ans;
+}
